b2bf83b940959edf8f475096698b2b10f63eb4da
Yuvaraj-96 <yuvarajmechmailam@gmail.com>
2022-01-02 00:24:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+import Card from './Card';
+import {useState} from 'react';
+
+function App() {
+
+
+  let free ={
+    title:'FREE',
+    price:0,
+    user: 'Single User',
+    userEnabler: true,
+    storage:'5GB Storage',
+    storageEnabler: true,
+    publicProject: 'Unlimited Public Projects',
+    publicProjectEnabler: true,
+    community:'Community Access',
+    communityEnabler: true,
+    privateProject:'Unlimited Private Projects',
+    privateProjectEnabler: false,
+    support:' Dedicated Phone Support',
+    supportEnabler:false,
+    domain:'Free Subdomain',
+    domainEnabler: false,
+    report:'Monthly Status Reports',
+    reportEnabler:false,
+  }
+
+  let PLUS ={
+    title:'PLUS',
+    price:9,
+    user: '5 Users',
+    userEnabler: true,
+    storage:'50GB Storage',
+    storageEnabler: true,
+    publicProject: 'Unlimited Public Projects',
+    publicProjectEnabler: true,
+    community:'Community Access',
+    communityEnabler: true,
+    privateProject:'Unlimited Private Projects',
+    privateProjectEnabler: true,
+    support:'Dedicated Phone Support',
+    supportEnabler:true,
+    domain:'Free Subdomain',
+    domainEnabler: true,
+    report:'Monthly Status Reports',
+    reportEnabler:false,
+  }
+
+  let PRO ={
+    title:'PRO',
+    price:49,
+    user: 'Unlimited Users',
+    userEnabler: true,
+    storage:'150GB Storage',
+    storageEnabler: true,
+    publicProject: 'Unlimited Public Projects',
+    publicProjectEnabler: true,
+    community:'Community Access',
+    communityEnabler: true,
+    privateProject:'Unlimited Private Projects',
+    privateProjectEnabler: true,
+    support:'Dedicated Phone Support',
+    supportEnabler:true,
+    domain:'Free Subdomains',
+    domainEnabler: true,
+    report:'Monthly Status Reports',
+    reportEnabler:true,
+  }
+
+  return (
+    <>
+      <section class="pricing py-5">
+        <div class="container">
+          <div class="row">
+            <Card type={free}/>
+            <Card type={PLUS}/>
+            <Card type={PRO}/>                               
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
